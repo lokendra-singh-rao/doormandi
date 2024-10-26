2bf41525234b1ee906bea73a390422ded00fc4f4
Lokendra Singh Rao <lokendras.715@gmail.com>
2024-10-26 23:47:44 +0530

    added responsiveness to footer

--- src/app/ui/Footer/Footer.tsx
+++ src/app/ui/Footer/Footer.tsx
@@ -11,45 +11,10 @@ export default function Footer() {
   };
 
   return (
-    <footer className="px-6 md:px-8 lg:px-12 w-full bg-gray-900 text-gray-300 pt-12 pb-8">
+    <footer className="px-8 md:px-10 lg:px-12 w-full bg-gray-900 text-gray-300 pt-12 pb-8">
       {/* Top Section */}
       <div className="mb-12">
-        <div className="flex items-center justify-between  gap-8">
-          {/* Newsletter */}
-          <div className="space-y-4">
-            <h3 className="text-xl font-semibold text-white">Get Fresh Updates</h3>
-            <form onSubmit={handleNewsletterSubmit} className="flex gap-2">
-              <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter your email" className="flex-1 rounded-lg border-gray-700 bg-gray-800 text-gray-300 shadow-inner px-4 py-2" />
-              <button type="submit" className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 transition">
-                Subscribe
-              </button>
-            </form>
-          </div>
-
-          {/* Social Media */}
-          <div className="space-y-4">
-            <h3 className="text-xl font-semibold text-white">Connect With Us</h3>
-            <div className="flex space-x-4">
-              <Facebook className="w-6 h-6 text-gray-400 hover:text-green-500 cursor-pointer transition" />
-              <Twitter className="w-6 h-6 text-gray-400 hover:text-green-500 cursor-pointer transition" />
-              <Instagram className="w-6 h-6 text-gray-400 hover:text-green-500 cursor-pointer transition" />
-            </div>
-          </div>
-
-          {/* App Downloads */}
-          <div className="space-y-4">
-            <h3 className="text-xl font-semibold text-white">Mobile App Coming Soon!</h3>
-            <div className="flex space-x-4">
-              <div className="w-32 h-12 bg-gray-700 rounded-lg flex items-center justify-center text-gray-400 hover:bg-gray-600 cursor-pointer transition">App Store</div>
-              <div className="w-32 h-12 bg-gray-700 rounded-lg flex items-center justify-center text-gray-400 hover:bg-gray-600 cursor-pointer transition">Play Store</div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* Main Content */}
-      <div className="mb-12">
-        <div className="flex items-center justify-between gap-8">
+        <div className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-start lg:justify-items-center items-center gap-12 xs:gap-16">
           {/* Company Info */}
           <div className="space-y-4">
             <h3 className="text-xl font-semibold text-white">Company</h3>
@@ -160,23 +125,55 @@ export default function Footer() {
           </div>
         </div>
       </div>
+      
+
+      {/* Main Content */}
+      <div className="mb-12">
+        <div className="flex flex-wrap items-center justify-center gap-12 md:gap-48">
+          {/* Newsletter */}
+          <div className="space-y-4">
+            <h3 className="text-xl font-semibold text-white">Newsletter</h3>
+            <form onSubmit={handleNewsletterSubmit} className="flex flex-wrap flex-col sm:flex-row gap-2">
+              <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter your email" className="flex-1 rounded-lg border-gray-700 bg-gray-800 text-gray-300 shadow-inner px-4 py-2" />
+              <button type="submit" className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 transition">
+                Subscribe
+              </button>
+            </form>
+            <p>🌱 Stay Fresh with Our Newsletter! 🍏</p>
+          </div>
+
+          {/* Social Media */}
+          <div className="space-y-4">
+            <h3 className="text-xl font-semibold text-white">Connect With Us</h3>
+            <div className="flex space-x-4">
+            <IconTooltip method="Facebook" title={"Facebook"} />
+            <IconTooltip method="Twitter" title={"Twitter"} />
+            <IconTooltip method="Instagram" title={"Instagram"} />
+            </div>
+          </div>
+
+          {/* App Downloads */}
+          {/* <div className="space-y-4">
+            <h3 className="text-xl font-semibold text-white">Mobile App Coming Soon!</h3>
+            <div className="flex space-x-4">
+              <div className="w-32 h-12 bg-gray-700 rounded-lg flex items-center justify-center text-gray-400 hover:bg-gray-600 cursor-pointer transition">App Store</div>
+              <div className="w-32 h-12 bg-gray-700 rounded-lg flex items-center justify-center text-gray-400 hover:bg-gray-600 cursor-pointer transition">Play Store</div>
+            </div>
+          </div> */}
+        </div>
+      </div>
 
       {/* Bottom Section */}
       <div className="border-t border-gray-800 pt-8">
-        <div className="flex items-center justify-between  gap-8">
+        <div className="flex flex-wrap items-center justify-evenly gap-8">
           {/* Copyright */}
-          <div className="text-gray-400 w-1/3">© {new Date().getFullYear()} Doormandi. All rights reserved.</div>
+          <div className="text-gray-400 text-center">© {new Date().getFullYear()} Doormandi. All rights reserved.</div>
 
-          {/* Payment Methods */}
-          <div className="flex items-center justify-center space-x-4 w-1/3">
-            <IconTooltip method="CreditCard" title={"Credit/Debit Card"} />
-            <IconTooltip method="Upi" title={"UPI"} />
-          </div>
-
-          {/* Certifications */}
-          <div className="flex justify-end space-x-4 w-1/3">
-            <Shield className="w-8 h-8 text-gray-400" />
-            <Truck className="w-8 h-8 text-gray-400" />
+          {/* Payment Methods & Certifications */}
+          <div className="flex items-center justify-center space-x-4">
+            <IconTooltip method="CreditCard" title={"Secure Checkout"} />
+            <IconTooltip method="Shield" title={"Data Protection"} />
+            <IconTooltip method="Truck" title={"Safe Delivery"} />
           </div>
         </div>
       </div>
--- src/app/ui/Icons/IconTooltip.tsx
+++ src/app/ui/Icons/IconTooltip.tsx
@@ -1,9 +1,14 @@
-import { CreditCard } from "lucide-react";
+import { CreditCard, Shield, Truck, Facebook, Twitter, Instagram } from "lucide-react";
 import Upi from "./Upi";
 
 const icons = {
   CreditCard,
   Upi,
+  Shield,
+  Truck,
+  Facebook,
+  Twitter,
+  Instagram,
 };
 
 interface IconTooltipProps {
@@ -16,7 +21,7 @@ export default function IconTooltip({ method, title }: IconTooltipProps) {
   return (
     <div className="relative flex justify-center items-center">
       <div className="group">
-        <IconComponent className="w-8 h-8 text-gray-400 group-hover:text-blue-500 cursor-pointer" />
+        <IconComponent className="w-8 h-8 text-gray-400 group-hover:text-green-500 cursor-pointer" />
         <div className="absolute left-1/2 transform -translate-x-1/2 bottom-8 hidden group-hover:flex items-center justify-center px-3 py-1 rounded bg-gray-800 text-white text-xs shadow-lg">{title}</div>
       </div>
     </div>
