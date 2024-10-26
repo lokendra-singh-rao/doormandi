89060574ebf315fee985c1df4db5ef5fb3af12a5
Lokendra Singh Rao <lokendras.715@gmail.com>
2024-10-26 19:23:52 +0530

    added quot character entity in testimonial section

--- src/app/ui/Home/CustomerTestimonials.tsx
+++ src/app/ui/Home/CustomerTestimonials.tsx
@@ -5,15 +5,15 @@ export default function CustomerTestimonials() {
       <div className="testimonials-grid grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
         <div className="testimonial bg-white shadow-md p-4 rounded-lg">
           <p>&quot;Amazing quality and fast delivery! Love the freshness.&quot;</p>
-          <h3 className="mt-2 font-semibold">- John D.</h3>
+          <h3 className="mt-2 font-semibold">- Jitendra S.</h3>
         </div>
         <div className="testimonial bg-white shadow-md p-4 rounded-lg">
           <p>&quot;Doormandi makes healthy eating so convenient.&quot;</p>
-          <h3 className="mt-2 font-semibold">- Sarah W.</h3>
+          <h3 className="mt-2 font-semibold">- Ayushi B.</h3>
         </div>
         <div className="testimonial bg-white shadow-md p-4 rounded-lg">
           <p>&quot;Great selection and very affordable prices!&quot;</p>
-          <h3 className="mt-2 font-semibold">- Mark T.</h3>
+          <h3 className="mt-2 font-semibold">- Karanveer B.</h3>
         </div>
       </div>
     </section>
